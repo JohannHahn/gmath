341f8213eb5092bf5abd349a847eb55901ad7daa
Johann Hahn <wanische@gmail.com>
2026-01-09 14:54:12 +0100

    some functions

--- gmath.hpp
+++ gmath.hpp
@@ -24,18 +24,30 @@ struct Vec3 {
         return sqrtf(x*x + y*y + z*z);
     }
 
-    std::string to_str() {
+    std::string to_str() const {
 	return std::string("Vec3: ") + std::to_string(x) + ", " + std::to_string(y) + ", " + std::to_string(z);
     }
 
     void multiply(const Mat4& m);
     void multiply(const Mat3& m);
 
-    void project() {
-	if (z == 0.f) return;
+    Vec3 project(int width, int height) const {
+	Vec3 v;
+	v.z = z;
+	if (v.z == 0.f) v.z = 0.00001f;
+
+	//std::println("projecting {} to width = {}, height = {}", to_str(), width, height);
+	float clip_x = x / v.z;
+	float clip_y = y / v.z;
+	//std::println("clipspace x = {}, y = {}", clip_x, clip_y);
 	
-	x /= z;
-	y /= z;
+	// move zero to top left and scale 
+	v.x = ((clip_x + 1.f) / 2.f) * width;
+	v.y = ((-clip_y + 1.f ) / 2.f) * height;
+
+	//std::println("final form = {}", v.to_str());
+
+	return v;
     }
 };
 
@@ -55,6 +67,7 @@ struct Vec4 {
     }
 
     void multiply(const Mat4& m);
+
 };
 
 struct Mat2 {
@@ -152,6 +165,8 @@ struct Mat3 {
     } 
 };
 
+Mat4 operator*(const Mat4& a, const Mat4& b);
+
 struct Mat4 {
     float data[4*4] = {0.f};
     int size = 4;
@@ -229,12 +244,45 @@ struct Mat4 {
 	         0.f, 0.f, 0.f, 1.f}};
     }
 
+    static Mat4 get_model(Vec3 pos, Vec3 angles) {
+	Mat4 m = identity();
+	m = translation(pos);
+	Mat4 rotation = rotation_x(angles.x) * rotation_y(angles.y) * rotation_z(angles.z);
+	m.multiply(rotation);
+	return m;
+    }
+
+    static Mat4 inverse(const Mat4 m) {
+
+	Mat4 r;
+
+	// Transponiere 3x3 Rotation
+	r.data[0] = m.data[0]; r.data[1] = m.data[4]; r.data[2] = r.data[8];
+	r.data[4] = m.data[1]; r.data[5] = m.data[5]; r.data[6] = r.data[9];
+	r.data[8] = m.data[2]; r.data[9] = m.data[6]; r.data[10]= r.data[10];
+
+	// Inverse Translation
+	float tx = m.data[12];
+	float ty = m.data[13];
+	float tz = m.data[14];
+
+	r.data[12] = -(r.data[0]*tx + r.data[4]*ty + r.data[8]*tz);
+	r.data[13] = -(r.data[1]*tx + r.data[5]*ty + r.data[9]*tz);
+	r.data[14] = -(r.data[2]*tx + r.data[6]*ty + r.data[10]*tz);
+
+	// letzte Zeile
+	r.data[15] = 1.0f;
+
+	return r;
+    }
+
 };
 
 template <typename T>
 T min (T a, T b) {
     return (a < b) ? a : b;
 }
+
 
 
 void add_inplace(Vec3& a, const Vec3& b);
@@ -250,6 +298,18 @@ Vec3 lerp(Vec3 start, Vec3 end, float t);
 float lerpf(float start, float end, float t);
 bool float_eq(float a, float b, float eps = 0.0000001f);
 float clamp(float t, float low, float high);
+
+Vec3 cross(const Vec3& a, const Vec3& b) {
+    return { a.y * b.z - a.z * b.y, 
+	     a.z * b.x - a.x * b.z,
+	     a.x * b.y - a.y * b.x };
+}
+
+template <typename T>
+bool in_range(T start, T end, T value) {
+    return value < end && value >= start;
+}
+
 
 
 #ifdef GMATH_IMPLEMENTATION
@@ -292,6 +352,25 @@ Vec3 operator/(const Vec3& v, float t) {
     return res;
 }
 
+Mat4 operator*(const Mat4& a, const Mat4& b) {
+
+    Mat4 res;
+
+    for (int row = 0; row < a.size; ++row) {
+	int start = row * a.size;
+
+	for (int col = 0; col < a.size; ++col) {
+	    res.data[col + row * a.size] = a.data[start] * b.data[col];
+
+	    for (int i = 1; i < a.size; ++i) {
+		res.data[col + row * a.size] += a.data[start + i] * b.data[col + a.size * i];
+	    }
+	}
+	
+    }
+    return res;
+}
+
 std::ostream& operator<<(std::ostream& outstr, const Vec3 v) {
 	outstr << v.x << " " << v.y << " " << v.z; 
 	return outstr;
@@ -321,8 +400,8 @@ Vec3 lerp(Vec3 start, Vec3 end, float t) {
 }
 
 float lerpf(float start, float end, float t) {
-	t = clamp(t, 0.f, 1.f);
-	//assert(t >= 0.f && t <= 1.f);
+	assert(t >= 0.f && t <= 1.f);
+	//t = clamp(t, 0.f, 1.f);
 	return start * (1.f - t) + end * t; 
 }
 
@@ -344,6 +423,8 @@ void Vec3::multiply(const Mat4& m) {
     int size = 3;
     float w = 1.f;
 
+    //std::println("before = {}", to_str());
+
     for (int i = 0; i < size; ++i) {
 	int start_mat = i * m.size;
 	data[i] = m.data[start_mat] * x + m.data[start_mat + 1] * y + m.data[start_mat + 2] * z + m.data[start_mat + 3] * w; 
@@ -351,6 +432,8 @@ void Vec3::multiply(const Mat4& m) {
     x = data[0];
     y = data[1];
     z = data[2];
+
+    //std::println("after = {}", to_str());
 }
 
 void Vec3::multiply(const Mat3& m) {
